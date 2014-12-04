716e7b13351069967965fc7bcdaff67a27cbd43e
Dr.Anonymous -Xinhao <anonymous@time-machine.tk>
2014-12-04 14:40:22 +0800

    Client

--- Client-REDAY.py
+++ Client-REDAY.py
@@ -1,7 +1,9 @@
 import webbrowser
 import os
-import md5
+import hashlib
 import json
+import urllib
+import urllib2
 print ('Welcome to use Time-Machine Client For Python')
 user=raw_input('请输入您的用户名:')
 password=raw_input('请输入您的密码:')
@@ -10,9 +12,18 @@ list= {
         'user' : user ,
         'password' : password
         }
-send=json.dumps(list)
-temp=send+key
-hash=md5.new(temp)
-print hash
-hash2=md5.degest(temp)
-print hash2
+json=json.dumps(list)
+send=json+key
+hash=hashlib.md5()
+hash.update(send)
+hash.hexdigest()
+req_url="http://www.time-machine.tk/api/client-login"
+send_data= {
+            'data':data,
+            'hash':hash
+            }
+send_data_urlencode=urllib.urlencode(send_data)
+connection=httplib.HTTPConnection("www.time-machine.tk")
+connection.request(method="POST",url=req_url,body=send_data_urlencode) 
+response=connection.getresponse()
+response_read=response.read()
